e2eef9b8d9e13d76885312f80e7851be483f5cb8
eldivino87 <peppeliccardo@gmail.com>
2015-03-18 17:22:00 +0100

    Update 16/03/15: * Niente

--- app/src/main/java/it/giuseppeliccardo/ionutrizionista/FragmentDatiAnagrafici.java
+++ app/src/main/java/it/giuseppeliccardo/ionutrizionista/FragmentDatiAnagrafici.java
@@ -1,7 +1,8 @@
 package it.giuseppeliccardo.ionutrizionista;
 
-import android.os.Bundle;
+import android.app.Activity;
 import android.app.Fragment;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,12 +17,33 @@ public class FragmentDatiAnagrafici extends Fragment {
         // Required empty public constructor
     }
 
+    // TODO: Interfaccia per la comunicazione con l'activity
+    public interface OnHeadlineSelectedListener {
+        public void onArticleSelected(int position);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         Log.i(TAG, getClass().getSimpleName() + ": entrato in onCreateView()");
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dati_anagrafici, container, false);
+    }
+
+
+    // TODO: Il fragment cattura l'interfaccia nell'onAttach
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        // This makes sure that the container activity has implemented
+        // the callback interface. If not, it throws an exception
+        try {
+            mCallback = (OnHeadlineSelectedListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnHeadlineSelectedListener");
+        }
     }
 
 }
